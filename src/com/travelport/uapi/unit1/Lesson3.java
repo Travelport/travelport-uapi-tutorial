a6b9a421504b35c69978ac38546857dfc143c96f
Ted Beatie <ted.beatie@travelport.com>
2014-05-29 13:13:58 -0700

    Copied src, wsdl

---
+++ src/com/travelport/uapi/unit1/Lesson3.java
@@ -0,0 +1,304 @@
+package com.travelport.uapi.unit1;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.ClientImpl;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
+
+import com.travelport.schema.air_v18_0.*;
+import com.travelport.schema.common_v15_0.PointOfSale;
+import com.travelport.schema.common_v15_0.ResponseMessage;
+import com.travelport.schema.rail_v12_0.RailPricingSolution;
+import com.travelport.service.air_v18_0.AirFaultMessage;
+import com.travelport.service.air_v18_0.AirLowFareSearchAsynchPortType;
+import com.travelport.tutorial.support.WSDLService;
+
+public class Lesson3 {
+	public static final String MYAPP = "tut";
+
+	public static void main(String[] argv) {
+		// pick a pair of cities that might be better to go via rail and have
+		// low cost carrier options
+		String origin = "GLA", destination = "LGW";
+		LowFareSearchAsynchReq req = new LowFareSearchAsynchReq();
+		// this creates the request parameters... and doesn't care if the
+		// request is synchronous or asynch
+		createLowFareSearchWithRail(req, origin, destination, 7, 9);
+
+		try {
+			System.out.println("waiting for first response from a provider...");
+			WSDLService.airShopAsync.showXML(true);
+			LowFareSearchAsynchRsp lowCostRsp = WSDLService.airShopAsync.get().service(req);
+			HashMap<String, Long> partMap = new HashMap<String, Long>();
+
+			List<AsyncProviderSpecificResponse> specificResponses = lowCostRsp
+					.getAsyncProviderSpecificResponse();
+
+			// print out what we got from initial response... this is to print
+			// the summary for all providers and set up the partMap for use in
+			// our loop below
+			for (Iterator<AsyncProviderSpecificResponse> specIter = specificResponses
+					.iterator(); specIter.hasNext();) {
+				AsyncProviderSpecificResponse asynchRsp = (AsyncProviderSpecificResponse) specIter
+						.next();
+				partMap.put(asynchRsp.getProviderCode(), asynchRsp
+						.getTotalParts().longValue());
+				System.out.println("Provider " + asynchRsp.getProviderCode()
+						+ " has a total of " + asynchRsp.getTotalParts()
+						+ " parts");
+			}
+
+			// prepare for the loop... we print first and ask for the response
+			// second we have to setup the values before entering the first time
+			String searchId = lowCostRsp.getSearchId();
+			String currentProvider = lowCostRsp.getProviderCode();
+			long currentPart = lowCostRsp.getPartNumber().longValue();
+			AirSearchRsp rsp = lowCostRsp; // so we can print it out
+			while (partMap.isEmpty() == false) {
+
+				System.out.println("++++++++++++++++++++\n"
+						+ "Response is from provider " + currentProvider + ":"
+						+ " part " + currentPart + " of "
+						+ partMap.get(currentProvider));
+
+				printSomeExampleResults(destination, rsp, 2);
+
+				long total = partMap.get(currentProvider);
+				if (total == currentPart) {
+					// finished with that one
+					partMap.remove(currentProvider);
+					// more providers?
+					if (partMap.isEmpty()) {
+						continue; // just get out of the loop
+					}
+					// change to next provider from the partMap
+					currentProvider = partMap.keySet().iterator().next();
+					currentPart = 1;
+				} else {
+					// more parts left on this provider
+					currentPart++;
+				}
+				// start the retreival of either the next part or the 1st part
+				// from the next provider
+
+				// just to show we can do something else while we wait
+				try {
+					System.out.println("Sleeping 5 secs before trying to "
+							+ "request part " + currentPart + " from "
+							+ currentProvider);
+					Thread.sleep(5 * 1000);
+				} catch (InterruptedException ignored) {
+					/* wont happen */
+				}
+				// sleep is finished, run the request for more data...
+				RetrieveLowFareSearchReq retrieve = new RetrieveLowFareSearchReq();
+				retrieve.setSearchId(searchId);
+				retrieve.setProviderCode(currentProvider);
+				retrieve.setPartNumber(BigInteger.valueOf(currentPart));
+				AirReq.addPointOfSale(retrieve, MYAPP);
+				WSDLService.airRetrieve.get();
+				rsp = WSDLService.airRetrieve.get().service(retrieve);
+				checkForErrorMessage(rsp);
+			}
+		} catch (AirFaultMessage e) {
+			System.err.println("Fault trying send request to travelport:"
+					+ e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Convenience routing for checking if there was an "expected" error from a
+	 * provider. If the error is not "expected" puke out with a runtime
+	 * exception
+	 * 
+	 * @param rsp
+	 *            the response object with the possible error
+	 * @return true if an error response message was found
+	 */
+	public static void checkForErrorMessage(AirSearchRsp rsp) {
+		boolean die = false;
+
+		List<ResponseMessage> msgs = rsp.getResponseMessage();
+		if (msgs.size() != 0) {
+			for (Iterator<ResponseMessage> iter = msgs.iterator(); iter
+					.hasNext();) {
+
+				ResponseMessage msg = (ResponseMessage) iter.next();
+				// no data available for a particular date or city pair?
+				if ((msg.getType().equals("Error"))
+						&& (msg.getCode().equals(BigInteger.ZERO))) {
+					if (msg.getProviderCode().equals(Helper.LOW_COST_PROVIDER)) {
+						System.out
+								.println("No data available from low cost provider?");
+						continue;
+					}
+				}
+
+				if (msg.getType().equals("Error")) {
+					die = true;
+				}
+				String supplier = "";
+				if (msg.getSupplierCode() != null) {
+					supplier = "[" + msg.getSupplierCode() + "] ";
+				}
+				System.out.print("Response Message From Provider "
+						+ msg.getProviderCode() + supplier + " : "
+						+ msg.getType() + " : " + msg.getValue() + " -- ");
+				System.out.println("Error Code = " + msg.getCode());
+			}
+		}
+		if (die) {
+			throw new RuntimeException("Unable to handle error from provider!");
+		}
+	}
+
+	/**
+	 * Just a way to display a few of the results returned in a response. There
+	 * are usually many solutions so we just display up to numberSamplesToShow.
+	 * Note the type here of the response is AirSearchRsp so it can be passed
+	 * either RetreiveLowFareSearchRsp or AsynchLowFareSearchRsp
+	 * 
+	 * @param destination
+	 *            the destination airport is needed for air solutions
+	 * @param rsp
+	 *            the response object
+	 * @param numberSamplesToShow
+	 *            the number of samples to show in this display
+	 */
+	public static void printSomeExampleResults(String destination,
+			AirSearchRsp rsp, int numberSamplesToShow) {
+		Helper.AirSegmentMap allAirSegments = null;
+		Helper.RailJourneyMap allRailJourneys = null;
+		Helper.RailSegmentMap allRailSegments = null;
+
+		// saves time if you don't care about seeing the examples
+		if (numberSamplesToShow == 0) {
+			return;
+		}
+
+		if (rsp.getAirSegmentList() != null) {
+			allAirSegments = Helper.createAirSegmentMap(rsp.getAirSegmentList()
+					.getAirSegment());
+		}
+		if (rsp.getRailSegmentList() != null) {
+			allRailJourneys = Helper.createRailJourneyMap(rsp
+					.getRailJourneyList().getRailJourney());
+			allRailSegments = Helper.createRailSegmentMap(rsp
+					.getRailSegmentList().getRailSegment());
+		}
+
+		int airTotal = rsp.getAirPricingSolution().size();
+		int railTotal = rsp.getRailPricingSolution().size();
+		int count = 0;
+
+		System.out.println("Total number solutions: " + airTotal + " air and "
+				+ railTotal + " rail");
+
+		// walk all the solutions and create a printable itinerary for each one
+		// then print them out
+		List<AirPricingSolution> airs = rsp.getAirPricingSolution();
+		for (Iterator<AirPricingSolution> iterator = airs.iterator(); iterator
+				.hasNext();) {
+			System.out.println("Example AIR Solution " + (count + 1) + " of "
+					+ airTotal);
+			AirPricingSolution soln = (AirPricingSolution) iterator.next();
+			PrintableItinerary i = new PrintableItinerary(soln, allAirSegments,
+					destination);
+			System.out.println(i + "\n-------------------");
+			++count;
+			if (count == numberSamplesToShow) {
+				break;
+			}
+		}
+		// print rail solutions
+		List<RailPricingSolution> rails = rsp.getRailPricingSolution();
+		for (Iterator<RailPricingSolution> iterator = rails.iterator(); iterator
+				.hasNext();) {
+			System.out.println("Example RAIL solution " + (count + 1) + " of "
+					+ railTotal);
+			RailPricingSolution soln = (RailPricingSolution) iterator.next();
+			PrintableItinerary i = new PrintableItinerary(soln,
+					allRailJourneys, allRailSegments);
+			System.out.println(i + "\n-------------------");
+			++count;
+			if (count == numberSamplesToShow) {
+				break;
+			}
+		}
+	}
+	/**
+	 * Update the low fare request in preparation for doing an asynchronous
+	 * request for low cost search.
+	 * 
+	 * @param origin
+	 * @param dest
+	 * @param departureDaysInFuture
+	 *            number of days from "now"
+	 * @param returnDaysInFuture
+	 *            number of days from "now"
+	 * @return
+	 */
+	public static void createLowFareSearchWithRail(
+			BaseLowFareSearchReq request, String originAirportcode,
+			String destAirportCode, int departureDaysInFuture,
+			int returnDaysInFuture) {
+
+		// add in the tport branch code
+		request.setTargetBranch(System.getProperty("travelport.targetBranch"));
+
+		// set the providers (including real and low cost) via a search modifier
+		AirSearchModifiers modifiers = AirReq.createModifiersWithProviders(
+				System.getProperty("travelport.gds"), 
+				Helper.RAIL_PROVIDER,
+				Helper.LOW_COST_PROVIDER);
+		request.setAirSearchModifiers(modifiers);
+
+		// need a POS as of v18_0
+		AirReq.addPointOfSale(request, MYAPP);
+
+		// we need to create a search leg but we do with some slack plus we use
+		// the city code for london
+		SearchAirLeg outbound = AirReq.createLeg(originAirportcode, destAirportCode);
+
+		AirReq.addDepartureDate(outbound,
+				Helper.daysInFuture(departureDaysInFuture));
+		AirReq.addEconomyPreferred(outbound);
+
+		// coming back, again something near these...
+		SearchAirLeg ret = AirReq.createLeg(destAirportCode, originAirportcode);
+		AirReq.addDepartureDate(ret, Helper.daysInFuture(returnDaysInFuture));
+		AirReq.addEconomyPreferred(ret);
+
+		// put them in the request
+		List<SearchAirLeg> legs = request.getSearchAirLeg();
+		legs.add(outbound);
+		legs.add(ret);
+
+		// one adult passenger
+		AirReq.addAdultPassengers(request, 1);
+
+		// get the point of sale right
+		PointOfSale gdsPOS = new PointOfSale();
+		gdsPOS.setProviderCode(System.getProperty("travelport.gds"));
+		gdsPOS.setPseudoCityCode(originAirportcode);
+
+		PointOfSale railPOS = new PointOfSale();
+		railPOS.setProviderCode(Helper.RAIL_PROVIDER);
+		railPOS.setPseudoCityCode(originAirportcode);
+		
+		PointOfSale lccPOS = new PointOfSale();
+		lccPOS.setProviderCode(Helper.LOW_COST_PROVIDER);
+		lccPOS.setPseudoCityCode(originAirportcode);
+		
+		request.getPointOfSale().add(gdsPOS);
+		request.getPointOfSale().add(railPOS);
+		request.getPointOfSale().add(lccPOS);
+	}
+}
---
+++ src/com/travelport/uapi/unit1/SystemSvcTest.java
@@ -0,0 +1,136 @@
+package com.travelport.uapi.unit1;
+
+/*
+ * I learn best by looking at and running tests.  A good set of tests shows
+ * how an API works and can't "lie" because they are executable.  These
+ * tests work on the 'System' service of travelport's uAPI. 
+ * 
+ * This test is designed to be used inside Eclipse with JUnit4 + Hamcrest
+ * matchers, although it will probably work in just about any java development 
+ * enviroment.  This has a dependency on Apache's CXF, version 2.6.0, 
+ * that is available for free at http://cxf.apache.org/
+ * 
+ * You need to change the arguments passed to the JVM to include 
+ * -Dtravelport.username and -Dtravelport.password to have your credentials.  
+ * You also must change the SYSTEM_WSDL URL below to point to a copy of the 
+ * system WSDL on your local filesystem.
+ */
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.ws.BindingProvider;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.travelport.schema.system_v8_0.PingReq;
+import com.travelport.schema.system_v8_0.PingRsp;
+import com.travelport.schema.system_v8_0.SystemInfoReq;
+import com.travelport.schema.system_v8_0.SystemInfoRsp;
+import com.travelport.schema.system_v8_0.TimeReq;
+import com.travelport.schema.system_v8_0.TimeRsp;
+import com.travelport.service.system_v8_0.SystemFaultMessage;
+import com.travelport.service.system_v8_0.SystemInfoPortType;
+import com.travelport.service.system_v8_0.SystemPingPortType;
+import com.travelport.service.system_v8_0.SystemService;
+import com.travelport.service.system_v8_0.SystemTimePortType;
+import com.travelport.tutorial.support.WSDLService;
+
+
+// Don't forget to add the -Dtravelport.username="AAA" and 
+//-Dtravelport.password="BBB" to your command line or run arguments in eclipse
+public class SystemSvcTest {
+	//definition of the format we get back as a string from tport
+    SimpleDateFormat tportDateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy");
+
+	@Test
+	public void testGetSystemtimePort() throws SystemFaultMessage, ParseException, InterruptedException {
+		TimeReq request = new TimeReq();
+		TimeRsp response;
+		
+		String traceId1 = "whocares-1982", traceId2="whocares-1968";
+		Date firstReqTime, secondReqTime, functionStart = new Date();
+		
+		//sleep a couple of seconds so functionStart will be different than the requests we run
+		//your clock needs to be pretty close to tports for this to work with only one
+		//second of difference!  make it bigger if your clocks are off by more...
+		Thread.sleep(1000);
+		
+		//use traceId for sanity
+		request.setTraceId(traceId1);
+		response = WSDLService.sysTime.get().service(request);
+		
+		//check sanity
+		assertThat(traceId1, is(equalTo(response.getTraceId())));
+		//parse string of response from tport into java object
+		firstReqTime = tportDateFormat.parse(response.getSystemTime());
+		//check that there is a difference between start of func and the result
+		assertThat(functionStart.before(firstReqTime), is(true));
+		
+		//sleep a couple of seconds
+		Thread.sleep(1000);
+		
+		//second request
+		request.setTraceId(traceId2);
+		response = WSDLService.sysTime.get().service(request);
+		//check sanity on second request
+		assertThat(traceId2, is(equalTo(response.getTraceId())));
+
+		//parse date returned
+		secondReqTime = tportDateFormat.parse(response.getSystemTime());
+		//check that there is a difference between 2nd call and 1st
+		assertThat(firstReqTime.before(secondReqTime), is(true));
+		
+		//if time is moving forward, the first time better less than the second!
+		long difference =  secondReqTime.getTime() - firstReqTime.getTime();
+		assertThat(difference > 0, is(true));
+		
+		//note:  response.getResponseTime() is the *length of time spent processing
+		//note:  the result by tport*, not the time of the response to this service! 
+	}
+	
+
+	@Test(expected=SystemFaultMessage.class)
+	public void testGetSystemInfoPort() throws SystemFaultMessage{
+		SystemInfoReq request = new SystemInfoReq();
+		
+		//no parameters to set on this request! we can take all the defaults??
+		WSDLService.sysInfo.get().service(request);
+		fail("we are expecting the line above to fail.  but still... why is " +
+				"thi line above not allowed?");
+	}
+
+	@Test
+	public void testGetSystemPingPort() throws SystemFaultMessage {
+		PingReq request = new PingReq();
+		PingRsp response;
+		
+		String myPayload= "this my payload; there are many like it but this one is mine";
+		String myTraceId = "doesntmatter-8176";
+
+		//set request params
+		request.setPayload(myPayload);
+		request.setTraceId(myTraceId);
+		
+		//run request through ping service
+		response = WSDLService.sysPing.get().service(request);
+		
+		//payload returned should be same as one sent
+		assertThat(myPayload, is(equalTo(response.getPayload())));
+		//sanity check with the trace id
+		assertThat(myTraceId, is(equalTo(response.getTraceId())));
+		// make suer that there is a transaction id even though we don't
+		// know what it will be exactly...
+		assertThat(response.getTransactionId().length() > 5, is(true));
+	}
+
+}
